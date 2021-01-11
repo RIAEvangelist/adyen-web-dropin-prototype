621b46f8d117ca6403a3e0a824cc47f22d7db18d
Brandon <brandon@diginow.it>
2021-01-10 19:16:31 -0800

    fixed weird capitolization issue that should have caused an error but didnt. maybe a bug in node

--- api/originKey.js
+++ api/originKey.js
@@ -1,10 +1,10 @@
 import {originKey} from '../creds/credentials.js';
 
-const getoriginKey=function(){
+const getOriginKey=function(){
     return {originKey}
 }
 
 export {
-    getoriginKey as default,
-    getoriginKey
+    getOriginKey as default,
+    getOriginKey
 }
